883285ce9c2c00509bdb6af0772b9d60b252169f
GregorStallmeister <post@gregor-stallmeister.de>
2025-02-03 16:35:35 +0100

    add comments and correct typos

--- src/main/java/PasswordValidation.java
+++ src/main/java/PasswordValidation.java
@@ -6,12 +6,13 @@ import java.util.Scanner;
  * A simple class with functions to validate passwords
  * It tests several criteria for secure passwords.
  * The user can input passwords to test them.
+ * Or the user can lat the program generate secure passwords.
  */
 public class PasswordValidation {
     private static final char[] specialCharacters = {'$', '%', '&', '!', '?', '*', '+', '~'};
 
     /**
-     *
+     * Provides a simple console to communicate with the user and calls the wanted function(s).
      * @param args - not used in function
      */
     public static void main(String[] args) {
@@ -22,25 +23,27 @@ public class PasswordValidation {
 
         while (! passwordToBeChecked.equals("q")) {
             System.out.println();
-            System.out.println("Please enter your password, which has to be checked or or c to create a password or q to quit: ");
+            System.out.println("Please enter your password, which has to be checked or c to create a password or q to quit: ");
             passwordToBeChecked = inputScanner.nextLine();
 
-            if (Objects.equals(passwordToBeChecked, "q"))
+            if (Objects.equals(passwordToBeChecked, "q")) // user wants to quit
                 continue;
 
-            if(Objects.equals(passwordToBeChecked, "c"))
+            if(Objects.equals(passwordToBeChecked, "c")) // user wants to let creating a password
             {
                 System.out.println(createSecurePassword());
                 continue;
             }
 
+            // user wants to check an inputted password
+
             System.out.println();
-            System.out.println("Your password meets (or not meets) the following criteria:");
+            System.out.println("Your password meets (or does not meet) the following criteria:");
             System.out.println("is at least 8 chars long: " + passwordContainsAtleast8Chars(passwordToBeChecked));
             System.out.println("contains at least 1 digit: " + passwordContainsDigits(passwordToBeChecked));
             System.out.println("contains both uppercase and lowercase: " + passwordContainsBothUppercaseAndLowercase(passwordToBeChecked));
             System.out.println("contains a commonly used password: " + passwordContainsCommonlyUsedPassword(passwordToBeChecked));
-            System.out.println("contains a special character: " + passwordContainsSpacialCharacter(passwordToBeChecked));
+            System.out.println("contains at least 1 special character: " + passwordContainsSpacialCharacter(passwordToBeChecked));
         }
 
     }
@@ -81,27 +84,29 @@ public class PasswordValidation {
         Random charCodeGenerator = new Random();
         char[] passwordChars = new char[16];
 
+        // Fill the array passwordChars with random values out of several kinds of chars
+
         for (int i = 0; i < 4; i++)
         {
-            int charCode = charCodeGenerator.nextInt(97, 123);
+            int charCode = charCodeGenerator.nextInt(97, 123); // a-z in ASCII-Table, decimal codes
             passwordChars[i] = (char)charCode;
         }
 
         for (int i = 3; i < 8; i++)
         {
-            int charCode = charCodeGenerator.nextInt(65, 91);
+            int charCode = charCodeGenerator.nextInt(65, 91); // A-Z in ASCII-Table, decimal codes
             passwordChars[i] = (char)charCode;
         }
 
         for (int i = 8; i < 12; i++)
         {
-            int charCode = charCodeGenerator.nextInt(0, specialCharacters.length -  1);
+            int charCode = charCodeGenerator.nextInt(0, specialCharacters.length -  1); // specialCharacters, initialized above
             passwordChars[i] = specialCharacters[charCode];
         }
 
         for (int i = 12; i < 16; i++)
         {
-            int charCode = charCodeGenerator.nextInt(48, 58);
+            int charCode = charCodeGenerator.nextInt(48, 58); // 0-9 in ASCII-Table, decimal codes
             passwordChars[i] = (char)charCode;
         }
 
@@ -117,10 +122,12 @@ public class PasswordValidation {
             passwordChars[j] = tempChar;
         }
 
+        // concat all the chars to a String
         StringBuilder passwordBuilder = new StringBuilder();
         for (char currentChar : passwordChars)
             passwordBuilder.append(currentChar);
 
+        // return the generated password
         return passwordBuilder.toString();
     }
 }
